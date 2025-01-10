1cb34723dabf5057097bfefa3f2bcaa48c43c6ab
EthanRossmath <ethanross981@gmail.com>
2025-01-10 15:04:08 -0500

    Fixing mistakes

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -38,26 +38,21 @@ ls data/raw #lists contents of data/raw
 mkdir data/processed #making a subdirectory named "processed" inside the directory data
 cd data/processed #changing directories to processed 
 mkdir server_logs user_logs event_logs #making three subdirectories in processed, "server_logs, user_logs, and event_logs"
-cd - #going back to the directory assignments
+cd ..
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 
- cp data/raw/server*.log data/processed/server_logs #moving all server.log files from server to server_logs
+ cp raw/server*.log processed/server_logs #moving all server.log files from server to server_logs
 
 # 6. Repeat the above step for user logs and event logs
 
- cp data/raw/user*.log data/processed/user_logs    #moving all user.log files from server to user_logs
- cp data/raw/event*.log data/processed/event_logs #moving all event.log files from server to event_logs
+ cp raw/user*.log processed/user_logs    #moving all user.log files from server to user_logs
+ cp raw/event*.log processed/event_logs #moving all event.log files from server to event_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-rf -rf ./data
-<<<<<<< HEAD
-
-rm data/raw/ipaddr* #removing all ipaddr files from raw
-rm data/raw/user_ipaddr* #removing user_ipadder files from raw
-rm data/processed/user_logs/user_ipaddr* #removing user_ipadder files from user_logs
-=======
->>>>>>> ea20676d33161a6f4d0fcd3c4f7aa5360f0f4309
+rm raw/ipaddr* #removing all ipaddr files from raw
+rm raw/user_ipaddr* #removing user_ipadder files from raw
+rm processed/user_logs/user_ipaddr* #removing user_ipadder files from user_logs
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 
